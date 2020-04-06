2b79e4610b10b5184eb212d8ab792ef3a7c51bfc
Jan Hagqvist <jaha100@gmail.com>
2020-04-06 03:59:13 +0300

    Initial commit

---
+++ Muistipeli/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Muistipeli
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblAika = new System.Windows.Forms.Label();
+            this.lblTaso = new System.Windows.Forms.Label();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.lblSiirrot = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "peitto.png");
+            this.imageList1.Images.SetKeyName(1, "kuva1.png");
+            this.imageList1.Images.SetKeyName(2, "kuva2.png");
+            this.imageList1.Images.SetKeyName(3, "kuva3.png");
+            this.imageList1.Images.SetKeyName(4, "kuva4.png");
+            this.imageList1.Images.SetKeyName(5, "kuva5.png");
+            this.imageList1.Images.SetKeyName(6, "kuva6.png");
+            this.imageList1.Images.SetKeyName(7, "kuva7.png");
+            this.imageList1.Images.SetKeyName(8, "kuva8.png");
+            this.imageList1.Images.SetKeyName(9, "kuva9.png");
+            this.imageList1.Images.SetKeyName(10, "kuva10.png");
+            this.imageList1.Images.SetKeyName(11, "kuva11.png");
+            this.imageList1.Images.SetKeyName(12, "kuva12.png");
+            this.imageList1.Images.SetKeyName(13, "kuva13.png");
+            this.imageList1.Images.SetKeyName(14, "kuva14.png");
+            this.imageList1.Images.SetKeyName(15, "kuva15.png");
+            this.imageList1.Images.SetKeyName(16, "kuva16.png");
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblAika
+            // 
+            this.lblAika.AutoSize = true;
+            this.lblAika.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAika.Location = new System.Drawing.Point(12, 9);
+            this.lblAika.Name = "lblAika";
+            this.lblAika.Size = new System.Drawing.Size(75, 31);
+            this.lblAika.TabIndex = 4;
+            this.lblAika.Text = "Aika:";
+            // 
+            // lblTaso
+            // 
+            this.lblTaso.AutoSize = true;
+            this.lblTaso.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTaso.Location = new System.Drawing.Point(378, 9);
+            this.lblTaso.Name = "lblTaso";
+            this.lblTaso.Size = new System.Drawing.Size(83, 31);
+            this.lblTaso.TabIndex = 4;
+            this.lblTaso.Text = "Taso:";
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.BackgroundImage = global::Muistipeli.Properties.Resources.leftarrow;
+            this.btnLeft.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnLeft.Location = new System.Drawing.Point(250, 9);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(60, 40);
+            this.btnLeft.TabIndex = 5;
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.BackgroundImage = global::Muistipeli.Properties.Resources.rightarrow;
+            this.btnRight.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnRight.Location = new System.Drawing.Point(316, 9);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(60, 40);
+            this.btnRight.TabIndex = 5;
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // lblSiirrot
+            // 
+            this.lblSiirrot.AutoSize = true;
+            this.lblSiirrot.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSiirrot.Location = new System.Drawing.Point(12, 40);
+            this.lblSiirrot.Name = "lblSiirrot";
+            this.lblSiirrot.Size = new System.Drawing.Size(105, 31);
+            this.lblSiirrot.TabIndex = 4;
+            this.lblSiirrot.Text = "Siirtoja:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 538);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.lblTaso);
+            this.Controls.Add(this.lblSiirrot);
+            this.Controls.Add(this.lblAika);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblAika;
+        private System.Windows.Forms.Label lblTaso;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Label lblSiirrot;
+    }
+}
+
---
+++ Muistipeli/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Muistipeli
+{
+    public partial class Form1 : Form
+    {
+        int usedtime = 0, valitut = 0, nayttoaika = -1;
+        int taso = 5, parejajaljella = 0, siirrot = 0;
+        int[,] kuvat = new int[5, 6];
+        int[,] nayttokuvat = new int[5, 6];
+        int[,] valittuna = new int[2, 2];
+        int[,] tasot = new int[,] { { 3, 2 }, { 2, 4 }, { 3, 4 }, { 4, 4 }, { 5, 4 }, { 5, 6 } };
+        MyPicBox[,] kuvaloota = new MyPicBox[5, 6];
+        System.Media.SoundPlayer sp1 = new System.Media.SoundPlayer();
+        System.Media.SoundPlayer sp2 = new System.Media.SoundPlayer();
+        System.Media.SoundPlayer sp3 = new System.Media.SoundPlayer();
+        Random r = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 5; i++) 
+            {
+                for (int j = 0; j < 6; j++) 
+                { // luodaan 5*6 pictureboxia eli kuvalootaa
+                    kuvaloota[i, j] = new MyPicBox(i, j);
+                    kuvaloota[i, j].Location = new System.Drawing.Point(10 + j * 90, 90 + i * 90);
+                    kuvaloota[i, j].Size = new System.Drawing.Size(80, 80);
+                    kuvaloota[i, j].SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+                    kuvaloota[i, j].Click += new System.EventHandler(kuvaloota_Click);
+                    this.Controls.Add(kuvaloota[i, j]);
+                }
+            }
+
+            sekoitaKuvat(); // sekoitetaan kuvat kuvalootiin
+            sp1.SoundLocation = "blip.wav"; // ladataan ääniefektit
+            sp2.SoundLocation = "nono.wav";
+            sp3.SoundLocation = "done.wav";
+            btnRight.Enabled = false; // aloitetaan suurimmalta tasolta, joten ei sallita isompaa tasoa
+            naytaKuvat(); // näytetään kuvalootien kuvat
+        }
+
+        private void kuvaloota_Click(object sender, EventArgs e)
+        {
+            MyPicBox pb = (MyPicBox)sender;
+
+            if ((nayttoaika == -1) && ((pb.y < tasot[taso, 0]) && (pb.x < tasot[taso, 1]))) // jos ollaan pelialueella ja väärän parin odotusaika loppu
+            { // sallitaan kuvien klikkailu
+                if (nayttokuvat[pb.y, pb.x] == 0) // jos klikattu suljettua kuvalootaa
+                {
+                    valittuna[valitut, 0] = pb.y; // talletetaan klikatun kuvan paikka
+                    valittuna[valitut, 1] = pb.x;
+                    kuvaloota[pb.y, pb.x].Image = imageList1.Images[kuvat[pb.y, pb.x]]; // näytetään kuva
+                    nayttokuvat[pb.y, pb.x] = kuvat[pb.y, pb.x]; // merkataan kuvaloota avatuksi
+                    valitut++; // valittuja kuvia on yksi enemmän
+
+                    if (valitut == 2) // jos kaksi kuvaa valittuna
+                    {
+                        siirrot++; // siirtojen määrä on yhtä suurempi
+
+                        if (kuvat[valittuna[0, 0], valittuna[0, 1]] == kuvat[valittuna[1, 0], valittuna[1, 1]]) // jos löytyi pari
+                        {
+                            parejajaljella--; // yksi pari vähemmän
+                            try
+                            {
+                                sp1.Play(); // soitetaan "pari löytyi" -ääni
+                            }
+                            catch { }
+
+                            if (parejajaljella == 0) // jos kaikki parit löydetty
+                                sp3.Play(); // soitetaan "kaikki parit löydetty" -ääni
+                        }
+                        else // kuvat olivat eriparia
+                        {
+                            nayttoaika = 10; // asetetaan sekunnin näyttöviive, jonka aikana ei voi klikkailla
+                            try
+                            {
+                                sp2.Play(); // soitetaan "väärä pari" -ääni
+                            }
+                            catch { }
+                        }
+
+                        valitut = 0; // valittuja kuvia ei ole
+                    }
+                }
+            }
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            btnRight.Enabled = true; // sallitaan tason lisäys
+            taso--; // helpommalle tasolle
+            if (taso < 1) // jos ollaan tasolla 0
+                btnLeft.Enabled = false; // ei enää alaspäin
+
+            sekoitaKuvat(); // tämän tason alustukset
+            naytaKuvat();
+            siirrot = 0;
+            usedtime = 0;
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            btnLeft.Enabled = true; // sallitaan tason vähennys
+            taso++; // vaikeammalle tasolle
+            if (taso > 4) // jos ollaan tasolla 5
+                btnRight.Enabled = false; // ei enää ylöspäin
+
+            sekoitaKuvat(); // tämän tason alustukset
+            naytaKuvat();
+            siirrot = 0;
+            usedtime = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int min, sec;
+
+            if (parejajaljella > 0) // jos kaikki parit eivät vielä löytyneet
+                usedtime++; // lisätään aikaa
+
+            min = usedtime / 600;
+            sec = (usedtime - min * 600) / 10;
+            lblAika.Text = "Aika: " + min + ":" + sec.ToString("00");
+            lblSiirrot.Text = "Siirtoja: " + siirrot;
+
+            if (nayttoaika >= 0) // jos väärän parin laskuria jäljellä
+                nayttoaika--; // vähennetään laskuria
+
+            if (nayttoaika == 0) // jos väärää paria näytetty tarpeeksi
+            { // piilotetaan väärän parin kuvat
+                kuvaloota[valittuna[0, 0], valittuna[0, 1]].Image = imageList1.Images[0];
+                nayttokuvat[valittuna[0, 0], valittuna[0, 1]] = 0;
+                kuvaloota[valittuna[1, 0], valittuna[1, 1]].Image = imageList1.Images[0];
+                nayttokuvat[valittuna[1, 0], valittuna[1, 1]] = 0;
+            }
+        }
+
+        private void naytaKuvat()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if ((i < tasot[taso, 0]) && (j < tasot[taso, 1])) // jos kuva pelialueella
+                        kuvaloota[i, j].Image = imageList1.Images[nayttokuvat[i, j]]; // näytetään kuvalootassa kuva tai suljettu luukku
+                    else // jos kuva ei pelialueella
+                        kuvaloota[i, j].Image = null; // ei näytetä kuvalootassa mitään
+                }
+            }
+
+            lblTaso.Text = "Taso: " + (taso + 1);
+        }
+
+        private void sekoitaKuvat()
+        {
+            parejajaljella = tasot[taso, 0] * tasot[taso, 1] / 2; // merkitään pareja jäljellä = luukkujen määrä / 2
+
+            for (int i = 0; i < tasot[taso, 0]; i++) 
+            {
+                for (int j = 0; j < tasot[taso, 1]; j++) 
+                {
+                    kuvat[i, j] = (i * 6 + j) / 2 + 1; // laitetaan pelialueella oleviin peräkkäisiin kuvalootiin parit
+                    nayttokuvat[i, j] = 0; // merkitään kuvalootan luukku suljetuksi
+                }
+            }
+
+            for (int i = 0; i < 100; i++) // sekoitetaan pelialueen kuvalootien kuvat sata kertaa
+            {
+                int tmp, x1, x2, y1, y2;
+
+                y1 = r.Next(0, tasot[taso, 0]);
+                x1 = r.Next(0, tasot[taso, 1]);
+                y2 = r.Next(0, tasot[taso, 0]);
+                x2 = r.Next(0, tasot[taso, 1]);
+                tmp = kuvat[y1, x1];
+                kuvat[y1, x1] = kuvat[y2, x2];
+                kuvat[y2, x2] = tmp;
+            }
+        }
+    }
+
+    public class MyPicBox : PictureBox
+    {
+        public int x, y;
+
+        public MyPicBox(int i, int j)
+        {
+            y = i;
+            x = j;
+        }
+    }
+}
